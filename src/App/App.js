94eb932f9de6caf9fe8d2c3b3f5a46bdb0241133
Alan Turing <austeyd@gmail.com>
2019-12-11 09:58:04 -0700

    Store fetch data in state

--- src/App/App.js
+++ src/App/App.js
@@ -6,14 +6,14 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      placeholder: []
+      reservations: []
     }
   }
 
   componentDidMount() {
     fetch('http://localhost:3001/api/v1/reservations')
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => this.setState({reservations: data}));
   }
 
 
