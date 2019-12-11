20bbd88cc1424b608b0ac7359bd5d91d917ab975
Alan Turing <austeyd@gmail.com>
2019-12-11 09:46:22 -0700

    Render Form component

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from '../Form/Form';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      placeholder: []
+    }
+  }
+
+
+
+
+
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+        <Form />
         </div>
         <div className='resy-container'>
-          
+
         </div>
       </div>
     )
