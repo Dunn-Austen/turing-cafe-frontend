6900a5e9d7d50992367f5acc726063004ce8ed1e
Alan Turing <austeyd@gmail.com>
2019-12-11 10:19:10 -0700

    Flesh out Reservations component with props data

--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -1,11 +1,25 @@
 
 import React from 'react';
 import './Reservations.css';
+import Card from '../Card/Card';
 
-const Reservations = () => {
+const Reservations = (props) => {
+  const reservationCards = props.reservations.map(booking => {
+    return (
+      <Card
+        name={booking.name}
+        date={booking.date}
+        time={booking.time}
+        number={booking.number}
+        key={booking.number}
+      />
+    )
+  })
+
+
   return (
     <div className="reservations">
-      Placeholder Text
+      {reservationCards}
     </div>
   )
 }
