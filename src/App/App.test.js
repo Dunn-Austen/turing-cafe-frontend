da019a9be24eea1a4228df6350ceccc43adb5f65
Alan Turing <austeyd@gmail.com>
2019-12-11 11:58:19 -0700

    Commit last bits as time runs out

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../App/App';
+import { shallow } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  it('should include a newReservation in App\'s state on btn click', () => {
+    const wrapper = shallow(<App />);
+    const reservationToBeAdded = {
+      name: "Christie",
+      date: "12/29",
+      time: "7:00",
+      number: 12,
+      key: "Christie12"
+    };
+
+    expect(wrapper.state('App')).toEqual([]);
+    wrapper.instance().addReservation(reservationToBeAdded);
+    expect(wrapper.state('App')).toEqual([reservationToBeAdded]);
+  });
 });
+
+
+{/*
+Property template
+
+name={booking.name}
+date={booking.date}
+time={booking.time}
+number={booking.number}
+key={`${booking.name}${booking.number}`}
+
+*/}
