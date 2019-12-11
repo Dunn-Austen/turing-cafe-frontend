0eaf7c9bddd80ecb43393e7e95e238bf23451a7c
Alan Turing <austeyd@gmail.com>
2019-12-11 09:45:36 -0700

    Establish Form component JSX scaffolding

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -4,6 +4,11 @@ import './Form.css';
 class Form extends Component {
   constructor() {
     super();
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      numGuests: '',
 
     }
   }
@@ -12,7 +17,31 @@ class Form extends Component {
   render() {
     return (
       <form>
-
+        <input
+          type='text'
+          placeholder='Name'
+          name='name'
+          value={this.state.name}
+        />
+        <input
+          type='text'
+          placeholder='Date (MM/DD)'
+          name='date'
+          value={this.state.date}
+        />
+        <input
+          type='text'
+          placeholder='Time'
+          name='time'
+          value={this.state.time}
+        />
+        <input
+          type='number'
+          placeholder='Number of Guests'
+          name='numGuests'
+          value={this.state.numGuests}
+        />
+        <button>Make Reservation</button>
       </form>
     )
   }
