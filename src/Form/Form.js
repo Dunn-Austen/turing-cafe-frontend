874862ad510ced164f43f6ac2d1d2b8e7415af11
Alan Turing <austeyd@gmail.com>
2019-12-11 09:44:33 -0700

    Establish Form component scaffolding

---
+++ src/Form/Form.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+  constructor() {
+    super();
+
+    }
+  }
+
+
+  render() {
+    return (
+      <form>
+
+      </form>
+    )
+  }
+}
+
+export default Form;
