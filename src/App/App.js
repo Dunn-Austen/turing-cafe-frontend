7f481e8f2d18d7808e783352dec72b9d24b7e5ea
Alan Turing <austeyd@gmail.com>
2019-12-11 11:45:54 -0700

    Add post functionality to addReservation method

--- src/App/App.js
+++ src/App/App.js
@@ -18,13 +18,19 @@ class App extends Component {
       .then(data => this.setState({reservations: data}));
   }
 
-
   addReservation = (newReservation) => {
     this.setState({reservations: [...this.state.reservations, newReservation]});
+    fetch('http://localhost:3001/api/v1/reservations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newReservation),
+    })
+       .then(response => response.json())
+       .then(data => console.log(data))
+       .catch(error => console.log('Can hypocrites credibly criticize?'));
   }
-
-
-
 
   render() {
     return (
